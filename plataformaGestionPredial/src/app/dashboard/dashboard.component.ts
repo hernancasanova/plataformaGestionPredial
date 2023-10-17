920cf58f7679a59ba6166dad07f68e980140df7e
Hernán Casanova <hernan.casanova.silva@outlook.com>
2023-10-17 08:32:20 -0300

    Actualización dashboard

--- plataformaGestionPredial/src/app/dashboard/dashboard.component.ts
+++ plataformaGestionPredial/src/app/dashboard/dashboard.component.ts
@@ -7,12 +7,14 @@ import { DashboardService } from './dashboard.service';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  public registers: Array<any> = [];
+  public bovines: Array<any> = [];
+  public identifiers: Array<any> = [];
   h:any={};
   constructor(private service: DashboardService) { }
 
   ngOnInit(): void {
-    this.loadRegisters();
+    this.loadBovines();
+    this.loadIdentifiers();
     // this.service.getRegisters().subscribe(r => {
     //   //return r.results;
     //   console.log("r: ",r);
@@ -21,9 +23,19 @@ export class DashboardComponent implements OnInit {
     // );
   }
 
-  async loadRegisters(): Promise<void> {
-    this.registers=await fetch("http://localhost:8006/listar").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
+  async loadBovines(): Promise<void> {
+    this.bovines=await fetch("http://localhost:8006/bovines").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
     //this.registers=await fetch("http://localhost:4000/listar").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
   }
+
+  async loadIdentifiers(): Promise<void> {
+    this.identifiers=await fetch("http://localhost:8007/identifiers").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
+    //this.registers=await fetch("http://localhost:4000/listar").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
+  }
+
+  // async loadRegisters(): Promise<void> {
+  //   this.registers=await fetch("http://localhost:8006/listar").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
+  //   //this.registers=await fetch("http://localhost:4000/listar").then(x=>x.json()).then(y=>y).catch(error=>console.log(error));
+  // }
 
 }
